c65b939d7ea156f071e317eee64ab97f39935248
Ravil <mohdravil000@gmail.com>
2020-12-14 11:45:17 +0530

    started react native

--- App.js
+++ App.js
@@ -1,16 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Appbar } from 'react-native-paper';
+import { StyleSheet, Text, View,Button } from 'react-native';
 
-export default function App() {
+class App extends React.Component 
+{
+  state = {
+    text:"loading....."
+  }
+  componentDidMount()
+  {
+    fetch("https://jsonplaceholder.typicode.com/users") 
+    .then(data=>data.json())
+    .then(data2=>
+      {
+        console.log(data2)
+        this.setState({
+          text:data2[0].name
+        })
+      })
+
+  }
+  handlestate()
+  {
+    this.setState({
+      name:"suresh"
+    })
+    console.log("updated_",this.state)
+  }
+  render()
+  {
+    // const arr=['Ravil','Shaikh','Jamia'];
+    // const val=arr.map(data =>
+    //   {
+    //   return <Text>{data}</Text>
+    //   })
+    
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <Text>{this.state.text}</Text>
       <StatusBar style="auto" />
     </View>
   );
+  }
 }
-
+export default App;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
